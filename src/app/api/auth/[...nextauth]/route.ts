1d24e1768a4d4e381c7ef3a5dc2b15adec3f00ec
flori <lucchini.florian.et21.21@gmail.com>
2025-07-31 20:59:28 -0300

    feat: update PostCSS configuration and remove unused SVG files
    - Changed PostCSS configuration to use object syntax for plugins.
    - Deleted unused SVG files: file.svg, globe.svg, next.svg, vercel.svg, and window.svg.
    - Updated layout import path for global styles.
    - Removed default Home page content and added a simple greeting in the new Home component.
    - Added Prisma schema for task and user models.
    - Implemented NextAuth authentication with credentials provider.
    - Created actions and loading files for log-in and register pages.
    - Added Prisma client initialization in a new prisma.ts file.
    - Established global CSS styles with Tailwind and custom properties.

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,57 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        mail: { label: "Email", type: "text", placeholder: "Email" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials) {
+        const res = await fetch("http://localhost:3000/api/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(credentials),
+        });
+
+        const user = await res.json();
+
+        if (res.ok && user) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/log-in",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.username = user.username;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id;
+      session.user.username = token.username;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
